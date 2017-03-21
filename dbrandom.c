6771c7d2acd6b3f7b4967859f4801fc466a92cee
Barret Rhoden <brho@cs.berkeley.edu>
2017-03-21 14:01:56 -0400

    Allow select() on /dev/urandom
    The kernel doesn't have support for FD taps on #random, but we actually
    don't need it.  select() will stat the FD first, and (currently) reading
     #random never blocks, so stat will always see the FD as readable.
    
    If we ever want to epoll, then we'll need tap support.
    
    Signed-off-by: Barret Rhoden <brho@cs.berkeley.edu>

--- dbrandom.c
+++ dbrandom.c
@@ -79,7 +79,6 @@ process_file(hash_state *hs, const char *filename,
 	{
 		int readlen, wantread;
 		unsigned char readbuf[4096];
-#ifndef __akaros__ // Can't select on Akaros's #random, and it never blocks
 		if (!already_blocked && !prngd)
 		{
 			int res;
@@ -98,7 +97,6 @@ process_file(hash_state *hs, const char *filename,
 				already_blocked = 1;
 			}
 		}
-#endif /* !__akaros__ */
 
 		if (len == 0)
 		{
